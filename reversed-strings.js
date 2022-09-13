74721734b3b0136dd7dcbf474f76851f35d0ffdd
Christian <89067580+Zopyros@users.noreply.github.com>
2022-09-12 20:14:02 -0700

    Update reversed-strings.js

---
+++ reversed-strings.js
@@ -0,0 +1,12 @@
+// Instructions
+
+//Complete the solution so that it reverses the string passed into it.
+// Example
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// Solution
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
