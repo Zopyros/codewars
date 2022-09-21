a0688309412c53d59b185f015d3664617db496d1
Christian <cmanalo.io@outlook.com>
2022-09-20 20:07:30 -0700

    Output string multiple times

---
+++ string-repeat.js
@@ -0,0 +1,14 @@
+// Instruction
+
+// Write a function that accepts an integer n and a string s as parameters, 
+// and returns a string of s repeated exactly n times.
+
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// My Solution
+
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
